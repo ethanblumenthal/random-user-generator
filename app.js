7ea8475b86ac39e9bdc8c6f8a31b61da6aeac0b2
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2017-12-28 22:48:23 -0600

    added fetch api for fullname

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 var url = 'https://randomuser.me/api/'
 var btn = document.querySelector('#btn');
+var fullname = document.querySelector('#fullname');
 
 btn.addEventListener('click', function() {
-
+    fetch(url)
+    .then(function(res) {
+        return res.json();
+    })
+    .then(function(data) {
+        fullname.innerText = data.results[0].name.first + ' ' + data.results[0].name.last;
+    })
+    .catch(function(err) {
+        console.log(err);
+    })
 });
