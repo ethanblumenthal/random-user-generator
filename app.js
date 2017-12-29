80b22b256d96173d92fa7a83918c7014b4f37fd9
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2017-12-28 23:01:57 -0600

    added error handling

--- app.js
+++ app.js
@@ -2,21 +2,39 @@ var url = 'https://randomuser.me/api/'
 var btn = document.querySelector('#btn');
 var fullname = document.querySelector('#fullname');
 var avatar = document.querySelector('#avatar');
+var username = document.querySelector('#username');
+var city = document.querySelector('#city');
+var email = document.querySelector('#email');
 
 btn.addEventListener('click', function() {
     fetch(url)
+    .then(handleErrors)
     .then(parseJSON)
-    .then(function(data) {
-        fullname.innerText = data.name.first + ' ' + data.name.last;
-        avatar.src = data.picture.medium;
-    })
-    .catch(function(err) {
-        console.log(err);
-    })
+    .then(updateProfile)
+    .catch(displayErrors)
 });
+
+function handleErrors(res) {
+    if (!res.ok) {
+        throw Error(res.status);
+    }
+    return res;
+}
 
 function parseJSON(res) {
     return res.json().then(function(data) {
         return data.results[0];
     });
+}
+
+function updateProfile(data) {
+    fullname.innerText = data.name.first + ' ' + data.name.last;
+    avatar.src = data.picture.medium;
+    username.innerText = data.login.username;
+    city.innerText = data.location.city;
+    email.innerText = data.email;
+}
+
+function displayErrors(err) {
+    console.log(err);
 }
